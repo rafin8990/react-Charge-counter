7fdee1a478d528aecab7312ae26f682544d9db19
rafin8990 <rafin8990@gmail.com>
2022-09-23 05:27:19 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1><Mobile></Mobile> </h1>
+      
     </div>
   );
 }
+
+
+
+const Mobile = () => {
+  const [charges ,setCharge]=useState(100)
+  
+    const Charge=()=>{
+      if(charges===0){
+        alert('Ekta Thappor Diya Dat Falai Dimo. Phone Charge a de')
+        // setCharge=charges
+        return
+      }
+      else{
+        const totalCharge = charges-10
+        setCharge(totalCharge)
+        }
+      }
+
+  return (
+    <div>
+      
+       <h1>Charges:{charges}%</h1>
+        <button onClick= {Charge}  className='battary'>Battary Down</button>
+      
+    </div>
+  );
+};
+
+
 
 export default App;
